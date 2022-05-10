553752f05d5edb6e78a99959c51cb6d81d98ce29
StianPost <stianpost@hotmail.com>
2022-05-10 12:15:53 +0200

    fixed booking more

--- components/Bookingmodal.tsx
+++ components/Bookingmodal.tsx
@@ -3,9 +3,26 @@ import * as Yup from 'yup';
 import { Field, Form, Formik } from 'formik';
 import React, { useState } from 'react';
 
+interface bookingDetails {
+  date: string;
+  people: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+
+interface bookingDetails {
+  date: string;
+  people: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+
 function Bookingmodal({ open, onClose, cabin }: any) {
   const [booked, setBooked] = useState(false);
   const [payment, setPayment] = useState(false);
+  const [isBooked, setIsBooked] = useState(false);
   const [bookingClosed, setBookingClosed] = useState(false);
   const [cardClosed, setCardClosed] = useState(true);
   const [bookingInfo, setBookingInfo] = useState({
@@ -15,12 +32,12 @@ function Bookingmodal({ open, onClose, cabin }: any) {
     lastName: '',
     email: '',
   });
-  console.log(bookingClosed);
+  const [paymentInfo, setPaymentInfo] = useState({});
   if (!open) return null;
 
   return (
     <>
-      <div className='overlay'></div>
+      <div className='modalOverlay'></div>
       <div className='modal'>
         <h3 className='text-center'>Checkout</h3>
         <div className='flex'>
@@ -32,7 +49,6 @@ function Bookingmodal({ open, onClose, cabin }: any) {
             <BookingInfo
               handleBooking={(val: any) => {
                 setBookingInfo(val);
-
                 setCardClosed(false);
               }}
               closed={() => {
@@ -41,13 +57,20 @@ function Bookingmodal({ open, onClose, cabin }: any) {
               open={bookingClosed}
             />
             <CardDetails
-              handleBooking={(val: any) => {
-                setBookingInfo(val);
+              handlePayment={(val: any) => {
+                setPaymentInfo(val);
+                setIsBooked(true);
               }}
               closed={() => {
                 setCardClosed(true);
               }}
               open={cardClosed}
+            />
+            <BookingMessage
+              paymentInfo={paymentInfo}
+              bookingInfo={bookingInfo}
+              open={isBooked}
+              cabin={cabin}
             />
             <button className='button button__primary' onClick={onClose}>
               Close
@@ -79,14 +102,6 @@ function BookingInfo({ handleBooking, closed, open }) {
       .required('Required'),
     date: Yup.date().required('You must pick a date!'),
   });
-
-  interface bookingDetails {
-    date: string;
-    people: number;
-    firstName: string;
-    lastName: string;
-    email: string;
-  }
 
   function onSubmit(val: bookingDetails) {
     handleBooking(val);
@@ -199,35 +214,37 @@ function BookingInfo({ handleBooking, closed, open }) {
   );
 }
 
-function CardDetails({ handleBooking, closed, open }) {
+function CardDetails({ handlePayment, closed, open }) {
   const [bookingInfo, setBookingInfo] = useState({});
   const SignupSchema = Yup.object().shape({
-    email: Yup.string().email('Invalid email').required('Required'),
-    firstName: Yup.string()
-      .min(2, 'Get a longer name!')
-      .max(15, 'Your name is too long!')
-      .required('You must have a first name'),
-    lastName: Yup.string()
-      .min(2, 'Get a longer name!')
-      .max(15, 'Your name is too long!')
-      .required('You must have a last name'),
-    people: Yup.number()
-      .min(1, 'There must be atleast 1 person!')
-      .max(25, 'Waaay too many!')
+    email: Yup.string().email('Invalid email').required('Email Required'),
+    paymentType: Yup.string().required('You must have a first name'),
+    price: Yup.number().required('You must pay a price'),
+    cardNumber: Yup.string()
+      .min(16, 'Invalid, too few chars')
+      .max(16, 'Invalid, too many characters')
       .required('Required'),
-    date: Yup.date().required('You must pick a date!'),
+    cvc: Yup.number()
+      .min(100, 'Too few characters')
+      .max(999, "That's too many characters")
+      .required('You must have a cvc/cvv number'),
+    expDate: Yup.string()
+      .min(6, "that's wrong")
+      .max(6, "that's too much wrong")
+      .required('you need an expiery date'),
   });
 
-  interface bookingDetails {
-    date: string;
-    people: number;
-    firstName: string;
-    lastName: string;
+  interface paymentDetails {
+    paymentType: string;
+    price: number;
+    cardNumber: string;
     email: string;
+    cvc: number;
+    expDate: string;
   }
 
   function onSubmit(val: bookingDetails) {
-    handleBooking(val);
+    handlePayment(val);
   }
   if (open) return null;
 
@@ -238,9 +255,10 @@ function CardDetails({ handleBooking, closed, open }) {
         initialValues={{
           paymentType: '',
           price: 0,
-          firstName: '',
-          lastName: '',
+          cardNumber: '',
           email: '',
+          cvc: '',
+          expDate: '',
         }}
         validationSchema={SignupSchema}
         onSubmit={(values) => {
@@ -261,6 +279,7 @@ function CardDetails({ handleBooking, closed, open }) {
                   type='paymentType'
                   className='w-full p-2 border-solid border-primary border-2 rounded-lg'
                 >
+                  <option value={''}></option>
                   <option value={'visa'}>Visa</option>
                   <option value={'masterCard'}>Mastercard</option>
                   <option value={'bitCoin'}>Bitcoin</option>
@@ -289,33 +308,48 @@ function CardDetails({ handleBooking, closed, open }) {
                 ) : null}
               </div>
             </div>
-            <div className='flex'>
+            <div className=''>
               <div>
-                <label htmlFor='firstName'>Firstname:</label>
+                <label htmlFor='cardNumber'>Card Number:</label>
                 <Field
-                  id='firstName'
-                  name='firstName'
-                  type='firstName'
+                  id='cardNumber'
+                  name='cardNumber'
+                  type='text'
                   className='w-full p-2 border-solid border-primary border-2 rounded-lg'
                 />
-                {errors.firstName && touched.firstName ? (
+                {errors.cardNumber && touched.cardNumber ? (
                   <div className='text-red-600 font-semibold'>
-                    {errors.firstName}
+                    {errors.cardNumber}
                   </div>
                 ) : null}
               </div>
+            </div>
+            <div className='flex'>
               <div>
-                <label htmlFor='lastName' className='mt-4'>
-                  Last name:
-                </label>
+                <label htmlFor='cvc'>cvc/cvv:</label>
                 <Field
-                  id='lastName'
-                  name='lastName'
+                  id='cvc'
+                  name='cvc'
+                  type='number'
                   className='w-full p-2 border-solid border-primary border-2 rounded-lg'
                 />
-                {errors.lastName && touched.lastName ? (
+                {errors.cvc && touched.cvc ? (
+                  <div className='text-red-600 font-semibold'>{errors.cvc}</div>
+                ) : null}
+              </div>
+              <div>
+                <label htmlFor='expDate' className='mt-4'>
+                  Exp Date:
+                </label>
+                <Field
+                  id='expDate'
+                  name='expDate'
+                  type='text'
+                  className='w-full p-2 border-solid border-primary border-2 rounded-lg'
+                />
+                {errors.expDate && touched.expDate ? (
                   <div className='text-red-600 font-semibold'>
-                    {errors.lastName}
+                    {errors.expDate}
                   </div>
                 ) : null}
               </div>
@@ -332,13 +366,26 @@ function CardDetails({ handleBooking, closed, open }) {
                 <div className='text-red-600 font-semibold'>{errors.email}</div>
               ) : null}
             </div>
-
+            <button type='button' onClick={() => {}}>
+              Back
+            </button>
             <button className='button button__primary mt-4' type='submit'>
-              Payment
+              Book
             </button>
           </Form>
         )}
       </Formik>
     </>
+  );
+}
+
+function BookingMessage({ paymentInfo, bookingInfo, open, cabin }) {
+  if (!open) return <div></div>;
+  return (
+    <div>
+      <h5>
+        Thank you {bookingInfo.firstName} for your booking {cabin.title}
+      </h5>
+    </div>
   );
 }
