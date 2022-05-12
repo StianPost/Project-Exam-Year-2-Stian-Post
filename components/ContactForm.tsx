e227e1d52be2d29b36fdc6583bb353cf82c06b8c
StianPost <stianpost@hotmail.com>
2022-05-12 20:06:32 +0200

    cleaned code

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,8 +1,11 @@
 import * as Yup from 'yup';
 
 import { Field, Form, Formik } from 'formik';
+import React, { useState } from 'react';
 
-import React from 'react';
+import { apiCall } from '../lib/const';
+import axios from 'axios';
+import { useRouter } from 'next/router';
 
 const SignupSchema = Yup.object().shape({
   email: Yup.string().email('Invalid email').required('Required'),
@@ -22,68 +25,87 @@ interface MyFormValueTypes {
   message: string;
 }
 
-const ContactForm = () => (
-  <>
-    <h3 className='text-center'>Contact</h3>
-    <Formik
-      initialValues={{
-        email: '',
-        subject: '',
-        message: '',
-      }}
-      validationSchema={SignupSchema}
-      onSubmit={(values) => {
-        // same shape as initial values
-        console.log(values);
-      }}
-    >
-      {({ errors, touched }) => (
-        <Form>
-          <div>
-            <label htmlFor='email'>Email*:</label>
-            <Field
-              id='email'
-              name='email'
-              type='email'
-              className='w-full p-2 border-solid border-primary border-2 rounded-lg'
-            />
-            {errors.email && touched.email ? (
-              <div className='text-red-600 font-semibold'>{errors.email}</div>
-            ) : null}
-          </div>
-          <div className='mt-2'>
-            <label htmlFor='subject' className='mt-4'>
-              Subject*:
-            </label>
-            <Field
-              id='subject'
-              name='subject'
-              className='w-full p-2 border-solid border-primary border-2 rounded-lg'
-            />
-            {errors.subject && touched.subject ? (
-              <div className='text-red-600 font-semibold'>{errors.subject}</div>
-            ) : null}
-          </div>
-          <div className='mt-2'>
-            <label htmlFor='message'>Message*:</label>
-            <Field
-              id='message'
-              name='message'
-              component='textarea'
-              placeholder='hallo'
-              className='w-full p-2 border-solid border-primary border-2 rounded-lg contactform__message'
-            />
-            {errors.message && touched.message ? (
-              <div className='text-red-600 font-semibold'>{errors.message}</div>
-            ) : null}
-          </div>
-          <button className='button button__primary mt-4' type='submit'>
-            Submit
-          </button>
-        </Form>
-      )}
-    </Formik>
-  </>
-);
+const ContactForm = () => {
+  const router = useRouter();
+  const [isError, setIsError] = useState(false);
+
+  const handleSubmit = async (values: any) => {
+    console.log(values);
+    try {
+      await axios.post(apiCall + '/contact-messages', values);
+      setIsError(false);
+    } catch (err: any) {
+      console.log(err.response.data.message);
+      setIsError(true);
+    }
+  };
+  return (
+    <>
+      <h3 className='text-center'>Contact</h3>
+      <Formik
+        initialValues={{
+          email: '',
+          subject: '',
+          message: '',
+        }}
+        validationSchema={SignupSchema}
+        onSubmit={(values) => {
+          // same shape as initial values
+          handleSubmit(values);
+        }}
+      >
+        {({ errors, touched }) => (
+          <Form>
+            <div>
+              <label htmlFor='email'>Email*:</label>
+              <Field
+                id='email'
+                name='email'
+                type='email'
+                className='w-full p-2 border-solid border-primary border-2 rounded-lg'
+              />
+              {errors.email && touched.email ? (
+                <div className='text-red-600 font-semibold'>{errors.email}</div>
+              ) : null}
+            </div>
+            <div className='mt-2'>
+              <label htmlFor='subject' className='mt-4'>
+                Subject*:
+              </label>
+              <Field
+                id='subject'
+                name='subject'
+                className='w-full p-2 border-solid border-primary border-2 rounded-lg'
+              />
+              {errors.subject && touched.subject ? (
+                <div className='text-red-600 font-semibold'>
+                  {errors.subject}
+                </div>
+              ) : null}
+            </div>
+            <div className='mt-2'>
+              <label htmlFor='message'>Message*:</label>
+              <Field
+                id='message'
+                name='message'
+                component='textarea'
+                placeholder='hallo'
+                className='w-full p-2 border-solid border-primary border-2 rounded-lg contactform__message'
+              />
+              {errors.message && touched.message ? (
+                <div className='text-red-600 font-semibold'>
+                  {errors.message}
+                </div>
+              ) : null}
+            </div>
+            <button className='button button__primary mt-4' type='submit'>
+              Submit
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </>
+  );
+};
 
 export default ContactForm;
--- lib/const.ts
+++ lib/const.ts
@@ -1,1 +1,8 @@
-export const apiCall:string = "http://localhost:1337/cabins"
+export const apiCall: string = 'http://localhost:1337/cabins';
+
+// export const headers = {
+//   headers: {
+//     'Content-Type': 'application/json',
+//     Authorization: `Bearer ${localStorage.getItem('jwt')}`,
+//   },
+// };
